19c973efbf5a843a023d9ab732f65b811cf6fc07
daniela.a.nikolova <daniela.a.nikolova@gmail.com>
2018-05-04 09:14:43 +0300

    GraphImplWithInteger

---
+++ Graph/com.seeburger.graph/src/graphWithInt/Graph.java
@@ -0,0 +1,100 @@
+package graphWithInt;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+public class Graph {
+	
+	private Map<Integer, List<Integer>> adjacencyList;
+	private List<Integer> verteces;
+	
+	public Graph() {
+		this.adjacencyList = new HashMap<>();
+		this.verteces = new LinkedList<>();
+	}
+	
+	public void addVertex(int vertexValue, List<Integer> currentVertexList) {
+			
+		this.adjacencyList.put(vertexValue, currentVertexList);
+		this.verteces.add(vertexValue);
+		
+	}
+	
+	public void BFSTraverse(int gate) {
+		
+		Queue<Integer> toVisit = new ArrayDeque<>();
+		List<Integer> resultList = new ArrayList<>();
+		
+		
+		resultList.add(gate);
+		
+		toVisit.addAll(adjacencyList.get(gate));
+		
+		
+		while (!toVisit.isEmpty()) 
+		{
+			
+			if (!resultList.contains(toVisit.peek())) 
+			{
+				toVisit.addAll(adjacencyList.get(toVisit.peek()));
+				resultList.add(toVisit.poll());
+			}else {
+				toVisit.poll();
+			}
+			
+		}
+		
+		System.out.println(resultList.toString());
+		
+		
+	}
+	
+	public void DFSTraversal(int gate) {
+		
+		
+		List<Integer> resutList = new LinkedList<>();
+		
+		getChildren(resutList, gate);		
+		
+		System.out.println(resutList.toString());
+		
+	}
+	
+	
+	private void getChildren(List<Integer> resutList, int gate) {
+		
+		resutList.add(gate);
+		Queue<Integer> toVisit = new ArrayDeque<>();
+		toVisit.addAll(adjacencyList.get(gate));
+		
+		while (toVisit!=null) {
+		
+				if (!resutList.contains(toVisit.peek())) {
+					if (!toVisit.isEmpty()) {
+						getChildren(resutList, toVisit.peek());
+					}
+					
+					
+				}else {
+					toVisit.poll();
+				}
+			
+			
+		}
+		
+		
+		
+		
+	}
+
+	public String mapInfo() {
+		return adjacencyList.toString();
+	}
+	
+
+}
---
+++ Graph/com.seeburger.graph/src/graphWithInt/Run.java
@@ -0,0 +1,53 @@
+package graphWithInt;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+
+
+public class Run {
+
+	public static void main(String[] args) throws IOException {
+		
+		
+		BufferedReader br = new BufferedReader(new FileReader("verteces.txt"));
+		
+		Graph myGraph = new Graph();
+		
+		String line = br.readLine();
+		
+		while (line!=null) {
+			
+			String[] vertexInfo = line.split(" -> ");
+			int vertexValue = Integer.parseInt(vertexInfo[0]);
+			List<Integer> adjacencyList = new LinkedList<>();
+						
+			String [] adjacencies = vertexInfo[1].split(", ");
+			
+			
+			for (int i = 0; i < adjacencies.length; i++) {
+				adjacencyList.add(Integer.parseInt(adjacencies[i]));
+				
+			}
+			
+			myGraph.addVertex(vertexValue, adjacencyList );
+			
+			line = br.readLine();
+		}
+		
+		br.close();
+		
+		myGraph.DFSTraversal(7);
+		
+//		myGraph.BFSTraverse(7);
+		
+//		System.out.println(myGraph.mapInfo());
+		
+		System.out.println();
+
+	}
+
+}
