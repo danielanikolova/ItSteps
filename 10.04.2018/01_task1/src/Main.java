364856f02d7e419749bb796a09cc32df6b30e865
Daniela Nikolova <d.nikolova@seeburger.com>
2018-08-15 11:53:02 +0300

    my daniela3

--- 10.04.2018/01_task1/src/Main.java
+++ 10.04.2018/01_task1/src/Main.java
@@ -27,14 +27,7 @@ public static void main(String[] args) throws IOException {
 			System.out.printf(arr[i] + " ");
 		}
 
-		for (int i = arr.length - 1; i >= 0; i++) {
-			System.out.printf(arr[i] + " ");
-		}
-
-		System.out.println("Here we are in daniela2");
-
-
-		System.out.println("Here we are in daniela branch");
+	 System.out.println("my new branch - daniela3");
 	}
 
 }
